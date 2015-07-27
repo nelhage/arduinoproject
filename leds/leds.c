01e3e285b5ef69abf7070d145da080afa72e6e98
Nelson Elhage <nelhage@nelhage.com>
2015-07-26 21:50:54 -0700

    checkpoint: rgb

--- leds/leds.c
+++ leds/leds.c
@@ -10,23 +10,22 @@
  * send square wave; each bit is high / low
  *
  * T0H = 350ns  ± 150ns  = 5.6 cycles
- * T0L = 800ns           = 12.8 cycles
  * T1H = 700ns           = 11.2 cycles
+ * T0L = 800ns           = 12.8 cycles
  * T1L = 600ns           = 9.6 cycles
  * TRESET = >50us
+ *
+ * GRB; MSB first
  */
 
-extern void lightit();
+extern void lightit(uint32_t rgb);
 
 int main(void)
 {
     DDRB = 0xFF;
     while(1) {
-        lightit();
-        PORTB |= _BV(PB2);
-        _delay_us(500);
-        PORTB &= ~_BV(PB2);
-        _delay_us(500);
+        lightit(0xc00000);
+        _delay_us(100);
     }
     return 0;
 }
