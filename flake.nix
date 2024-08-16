684614cd9905cbf1476f48b8dce78fa486f06db2
Nelson Elhage <nelhage@nelhage.com>
2024-08-16 15:41:01 -0700

    Add a flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "direnv development template";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.pkgsCross.avr.mkShell {
+            packages = [
+              pkgs.avrdude
+            ];
+          };
+        }
+      );
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
+    };
+}
