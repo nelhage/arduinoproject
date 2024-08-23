26d9a0d7fea9575b0a9c95b51cece89519656bc0
Nelson Elhage <nelhage@nelhage.com>
2024-08-23 09:25:51 -0700

    switch jumper pins

--- heart/heart.c
+++ heart/heart.c
@@ -11,18 +11,16 @@ int main(void)
 {
     DDRB |= _BV(PINB0);
 
-    // 0 = PD0, 1 = PD1
-
-    DDRD &= ~(_BV(PIND0) | _BV(PIND1));
+    DDRB &= ~(_BV(PINB1) | _BV(PINB2));
     // set pullup
-    PORTD |= (_BV(PIND0) | _BV(PIND1));
+    PORTB |= (_BV(PINB1) | _BV(PINB2));
 
     int16_t t = 0;
 
     for(;;) {
         t++;
 
-        tick(PIND & 0x3, t);
+        tick((PINB & (0x3 << 1)) >> 1, t);
         lightit(colors, sizeof(colors));
         _delay_ms(100);
     }
