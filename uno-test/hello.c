e2009808c51a0c841049b96242955aec51af3c95
Nelson Elhage <nelhage@nelhage.com>
2026-04-30 18:51:37 -0700

    RGB controls

--- uno-test/hello.c
+++ uno-test/hello.c
@@ -19,8 +19,9 @@ static void adc_init(void)
   ADCSRA = _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
 }
 
-static uint8_t adc_read(void)
+static uint8_t adc_read(uint8_t channel)
 {
+  ADMUX = (ADMUX & ~0x0f) | (channel & 0x0f);
   ADCSRA |= _BV(ADSC);
   loop_until_bit_is_clear(ADCSRA, ADSC);
   return ADCH;
@@ -28,7 +29,7 @@ static uint8_t adc_read(void)
 
 int main(void)
 {
-  DDRB |= _BV(PB5) | _BV(PB0);
+  DDRB |= _BV(PB5) | _BV(PB1) | _BV(PB0);
 
   uart_init();
   uart_setup_stdio(NULL, &uart_output);
@@ -36,16 +37,24 @@ int main(void)
 
   adc_init();
 
-  int i;
-  for (i = 0; i < 10; i ++) {
-      leds[i].r = 100;
-  }
-  lightit(leds, NLED);
+  _delay_ms(2000);
 
   while (1) {
-    PORTB ^= _BV(PB5);
-    printf("ADC0: %u\n", adc_read());
-    _delay_ms(1000);
+    PORTB |= _BV(PB1);
+    _delay_us(100);
+    uint8_t r = adc_read(0);
+    uint8_t g = adc_read(1);
+    uint8_t b = adc_read(2);
+    PORTB &= ~_BV(PB1);
+
+    for (int i = 0; i < NLED; i++) {
+      leds[i].r = r;
+      leds[i].g = g;
+      leds[i].b = b;
+    }
+    lightit(leds, NLED);
+
+    _delay_ms(50);
   }
 
   return 0;
